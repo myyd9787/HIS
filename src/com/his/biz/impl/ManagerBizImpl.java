16e9d994145913864c56a4fb100e7cd40e61b6f7
keep-snow <1799813043@qq.com>
2019-08-29 10:13:29 +0800

    zl

---
+++ src/com/his/biz/impl/ManagerBizImpl.java
@@ -0,0 +1,13 @@
+package com.his.biz.impl;
+
+import com.his.biz.ManagerBiz;
+import com.his.entity.ConstantType;
+
+import java.util.List;
+
+public class ManagerBizImpl implements ManagerBiz {
+    @Override
+    public List<ConstantType> showAll() {
+        return null;
+    }
+}
